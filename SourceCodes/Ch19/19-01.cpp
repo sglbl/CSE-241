d01e891f8bb9d3480813252f89b1389daf1cd241
Suleyman <silo1881@gmail.com>
2023-01-27 20:09:32 +0300

    Adding source codes

---
+++ SourceCodes/Ch19/19-01.cpp
@@ -0,0 +1,35 @@
+//Program to demonstrate STL iterators.
+#include <iostream>
+#include <vector>
+using namespace std;
+int main( )
+{
+    vector<int> container;
+
+    for (int i = 1; i <= 4; i++)
+        container.push_back(i);
+
+    cout << "Here is what is in the container:\n";
+    vector<int>::iterator p;
+    for (p = container.begin( ); p != container.end( ); p++)
+        cout << *p << " ";
+    cout << endl;
+
+    cout << "Setting entries to 0:\n";
+    for (p = container.begin( ); p != container.end( ); p++)
+         *p = 0;
+    cout << "Container now contains:\n";
+ 
+    for (p = container.begin( ); p != container.end( ); p++)
+        cout << *p << " ";
+    cout << endl;
+
+    return 0;
+}
+/*
+Here is what is in the container:                                                                                                                                                                                                                      
+1 2 3 4                                                                                                                                                                                                                                                
+Setting entries to 0:                                                                                                                                                                                                                                  
+Container now contains:                                                                                                                                                                                                                                
+0 0 0 0    
+*/
---
+++ SourceCodes/readme.txt
@@ -0,0 +1,3 @@
+You can use this website to download a spesific folder from a ssh/ftp server on the web.
+https://sourceforge.net/projects/visualwget/
+-suleyman
